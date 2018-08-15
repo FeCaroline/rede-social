78312bf787c9cd4508cd1fc9309a8e37a57f4a27
Thalissa Jenniffer <thalissajenniffer@hotmail.com>
2018-08-15 15:05:20 -0300

    bug login

--- login.js
+++ login.js
@@ -1,15 +1,26 @@
+var config = {
+  apiKey: "AIzaSyCCDmW8UvUpzHbPo7p-h0UcTcy8Nkm3SYk",
+  authDomain: "snout-book-lab.firebaseapp.com",
+  databaseURL: "https://snout-book-lab.firebaseio.com",
+  projectId: "snout-book-lab",
+  storageBucket: "snout-book-lab.appspot.com",
+  messagingSenderId: "179038112155"
+};
+
+firebase.initializeApp(config);
+
 $(document).ready(function(){
-  $("sig-up-button").click(function(event){
+  $("#cadButton").click(function(event){
     event.preventDefault();
 
-
-    var email = $(".sign-up-email").val();
-    var password = $(".sign-up-password").val();
+    var name = $("#nameCad").val();
+    var email = $("#emailCad").val();
+    var password = $("#passwordCad").val();
 
     firebase.auth().createUserWithEmailAndPassword(email, password)
     .then(function(){
       window.location="index.html";
-
+      name: name
 
     })
     .catch(function(error) {
@@ -20,12 +31,12 @@ $(document).ready(function(){
     });
 
   })
-  $("sign-in-button").click(function(event){
+  $("#loginButton").click(function(event){
     event.preventDefault();
 
 
-    var email = $(".sign-in-email").val();
-    var password = $(".sign-in-password").val();
+    var email = $("#passwordLogin").val();
+    var password = $("#emailLogin").val();
 
     firebase.auth().signInWithEmailAndPassword(email, password)
     .then(function(){
