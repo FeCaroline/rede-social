bc8418c009c99e641bd19ebab98b610288d297e9
Thalissa Jenniffer <thalissajenniffer@hotmail.com>
2018-08-16 05:00:47 -0300

    problemas em remover os itens da lista

--- app.js
+++ app.js
@@ -1,16 +1,44 @@
 var database = firebase.database();
+var USER_ID = window.location.search.match(/\?userId=(.*)/)[1];
 
-$(document).ready(function(){
-  $(".post-button".click(function(event){
+var typePost = "public";
+$(".public-post").click(function(event){
+  typePost = "public";
+});
+$(".friends-post").click(function(event){
+  typePost = "friends";
+});
+
+$(document).ready(function() {
+
+  database.ref('/posts/' + USER_ID).once('value').then(function(snapshot) {
+    snapshot.forEach(function(childSnapshot) {
+    var childKey = childSnapshot.key;
+    var childData = childSnapshot.val();
+    createPost(childData.text, childKey);
+  });
+});
+
+  $(".button-post").click(function(event){
     event.preventDefault();
 
-    var text =  $(".post").val();
-    $(".post").val("");
-    $(".user-posts").html(<ul>${text}</ul>);
-
-    database.ref('post/').push({
-      text:text
-
-    });
-  })
+    var text = $(".input-post").val();
+    if(text !== ""){
+      $(".input-post").val("");
+      var newPostDB = database.ref('posts/' + USER_ID).push({
+        text: text,
+        type: typePost
+      });
+      createPost(text, newPostDB.key);
+    }
+  });
 });
+function createPost(text, key){
+  $(".post-place").append('<li><span>"${text}"/span><button class="edit-post" data-edit-id="${key}"><i class="fas fa-edit"></i></button><button class="delete-post" data-delete-id="'"+key+"'"><i class="fas fa-trash-alt"></i></button></li>');
+  $("button[data-delete-id="'"+key+"'"]").click(function(){
+    console.log(text);
+  })
+  $(".edit-post").click(function(){
+    console.log("general");
+  })
+}
