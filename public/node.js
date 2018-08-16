456a12db2833bcafbe386a29a3e916c494f81fbb
Fernanda Caroline <fe.caroline90@gmail.com>
2018-08-16 17:50:19 -0300

    0.7%

---
+++ public/node.js
@@ -0,0 +1,9 @@
+var config = {
+  apiKey: "AIzaSyCCDmW8UvUpzHbPo7p-h0UcTcy8Nkm3SYk",
+  authDomain: "snout-book-lab.firebaseapp.com",
+  databaseURL: "https://snout-book-lab.firebaseio.com",
+  projectId: "snout-book-lab",
+  storageBucket: "snout-book-lab.appspot.com",
+  messagingSenderId: "179038112155"
+};
+firebase.initializeApp(config);
